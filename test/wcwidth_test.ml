77f55a6f7f35002d7fb59d2690ab8aa9c65a79e8
Jonathan Yong <yongrenjie@gmail.com>
2024-01-19 17:00:01 +0000

    Rename tests->test so that dune picks it up

--- test/wcwidth_test.ml
+++ test/wcwidth_test.ml
@@ -9,5 +9,6 @@ let () =
       test_case "ASCII" `Quick (test_wcswidth "Hello, Tom!" 11);
       test_case "Empty string" `Quick (test_wcswidth "" 0);
       test_case "Newline" `Quick (test_wcswidth "\n" (-1));
+      test_case "Swan emoji" `Quick (test_wcswidth "🦢" 2);
     ];
   ]
