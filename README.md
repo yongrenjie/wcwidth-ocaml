b1dbe7cf480fa7fd7ebb430b6d84885c4fbd9b84
Jonathan Yong <yongrenjie@gmail.com>
2024-04-19 15:42:38 +0100

    Set up script to generate character lists

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # wcwidth
 
 Implementation of the `wcwidth(3)` and `wcswidth(3)` functions for OCaml, which lets you calculate the number of columns a string occupies in a terminal.
+
+## Update the character lists
+
+The Unicode version the library uses is documented in `wcwidth/char_list.ml`.
+
+This file also contains lists of zero-width and wide characters, and is derived from the Python [`wcwidth` package](https://pypi.org/project/wcwidth/).
+This file can be updated (e.g. to use a new Unicode version) using the Python script `setup_char_lists.py`:
+
+```
+python -m venv .venv
+source .venv/bin/activate
+python -m pip install wcwidth
+python setup_char_lists.py
+```
---
+++ setup_char_lists.py
@@ -0,0 +1,37 @@
+from wcwidth.table_zero import ZERO_WIDTH
+from wcwidth.table_wide import WIDE_EASTASIAN
+
+from pathlib import Path
+
+UNICODE_VERSION = '15.1.0'
+
+OUTPUT_FILE = Path(__file__).parent / "wcwidth" / "char_list.ml"
+
+zero_width_tuples = "\n  ; ".join([
+    f"0x{t[0]:05x}, 0x{t[1]:05x}"
+    for t in ZERO_WIDTH[UNICODE_VERSION]
+])
+
+wide_tuples = "\n  ; ".join([
+    f"0x{t[0]:05x}, 0x{t[1]:05x}"
+    for t in WIDE_EASTASIAN[UNICODE_VERSION]
+])
+
+with open(OUTPUT_FILE, "w") as f:
+    print(f"""
+(** Zero-width characters as of Unicode {UNICODE_VERSION}.
+    Taken from the Python wcwidth package:
+    https://pypi.org/project/wcwidth/ *)
+let zero_width_chars =
+  [ {zero_width_tuples}
+  ]
+;;
+
+(** Wide characters as of Unicode {UNICODE_VERSION}.
+    Taken from the Python wcwidth package:
+    https://pypi.org/project/wcwidth/ *)
+let wide_chars =
+  [ {wide_tuples}
+  ]
+;;
+""".strip(), file=f)
